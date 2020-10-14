ed00d6c2bbbf429278c7eeb1867e98e755387592
Sean <mulkasea@msu.edu>
2020-10-14 13:22:42 -0400

    Name

--- app/src/main/java/edu/msu/hagopi10/project1/MainActivity.java
+++ app/src/main/java/edu/msu/hagopi10/project1/MainActivity.java
@@ -2,7 +2,9 @@ package edu.msu.hagopi10.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
