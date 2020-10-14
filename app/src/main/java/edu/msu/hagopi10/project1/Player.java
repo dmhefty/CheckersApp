8cb65e3eea9e51ab2fbc0cacde99465fbc023818
Sean <mulkasea@msu.edu>
2020-10-14 17:10:38 -0400

    Name

--- app/src/main/java/edu/msu/hagopi10/project1/Player.java
+++ app/src/main/java/edu/msu/hagopi10/project1/Player.java
@@ -1,8 +1,19 @@
 package edu.msu.hagopi10.project1;
-
+import android.provider.Settings;
+import java.lang.String;
+import java.util.ArrayList;
 public class Player {
-
+    //array of pieces a player has
+    public ArrayList<CheckerPiece> pieces = new ArrayList<CheckerPiece>();
+    // how many pieces the player has left
 private float score;
+private String name;
 
 
+public Player(String name, ArrayList checkers, float score1){
+    this.score = score1;
+    this.pieces = checkers;
+    this.name = name;
+
+}
 }
