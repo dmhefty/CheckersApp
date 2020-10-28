128ac8145b52764f3b91a6da31a981f87de50355
Sean <mulkasea@msu.edu>
2020-10-28 16:01:03 -0400

    Name

--- app/src/main/java/edu/msu/hagopi10/project1/MainActivity.java
+++ app/src/main/java/edu/msu/hagopi10/project1/MainActivity.java
@@ -1,22 +1,45 @@
 package edu.msu.hagopi10.project1;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int  ENTER_NAMES = 1;
+    public static final String NAME1 = "Player1";
+    public static final String NAME2= "Player2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onStartNameActivity(View view) {
-        Intent intent = new Intent(this, NameActivity.class);
+
+    public void onStartCheckersActivity(View view) {
+
+        Intent intent1 = new Intent();
+        EditText editText1 = (EditText) findViewById(R.id.PLAYER1NAME);
+        EditText editText2 = (EditText) findViewById(R.id.PLAYER2NAME);
+        intent1.putExtra(NAME1, editText1.getText().toString());
+        intent1.putExtra(NAME2, editText2.getText().toString());
+        Intent intent = new Intent(this, CheckersActivity.class);
         startActivity(intent);
     }
 
+        @Override
+        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+            super.onActivityResult(requestCode, resultCode, data);
+
+        }
 
 }
